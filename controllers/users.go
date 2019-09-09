8ff2937686169638d53e86444f6667d49162cf99
Adan Gutierrez <hevyw8@gmail.com>
2019-09-08 23:43:30 -0500

    added user controller

---
+++ controllers/users.go
@@ -0,0 +1,15 @@
+package controllers
+
+import (
+	"github.com/aggdotred/lenslocked.com/views"
+)
+
+func NewUsers() *Users {
+	return &Users{
+		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
+	}
+}
+
+type Users struct {
+	NewView *views.View
+}
