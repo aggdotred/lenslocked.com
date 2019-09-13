3f155be7bb46771891704109e007f6bd9678d88b
Adan Gutierrez <hevyw8@gmail.com>
2019-09-13 18:53:48 -0500

    implemented postgres and wired db up to user model

--- controllers/users.go
+++ controllers/users.go
@@ -4,21 +4,25 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/aggdotred/lenslocked.com/models"
 	"github.com/aggdotred/lenslocked.com/views"
 )
 
 type Users struct {
 	NewView *views.View
+	us      *models.UserService
 }
 
 type SignupForm struct {
+	Name     string `schema:"name"`
 	Email    string `schema:"email"`
 	Password string `schema:"password"`
 }
 
-func NewUsers() *Users {
+func NewUsers(us *models.UserService) *Users {
 	return &Users{
 		NewView: views.NewView("bootstrap", "users/new"),
+		us:      us,
 	}
 }
 
@@ -35,11 +39,17 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 //
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-
 	var form SignupForm
 	if err := parseForm(r, &form); err != nil {
 		panic(err)
 	}
-	fmt.Fprintln(w, "Email is", form.Email)
-	fmt.Fprintln(w, "Password is", form.Password)
+	user := models.User{
+		Name:  form.Name,
+		Email: form.Email,
+	}
+	if err := u.us.Create(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintln(w, "User is", user)
 }
--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/aggdotred/lenslocked.com/controllers"
+	"github.com/aggdotred/lenslocked.com/models"
+
 	"github.com/gorilla/mux"
 )
 
+const (
+	host   = "localhost"
+	port   = 5432
+	user   = "postgres"
+	dbname = "postgres"
+)
+
 func main() {
+	// Create a DB connection string and
+	// use it to create our model services.
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable", host, port, user, dbname)
+	us, err := models.NewUserService(psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer us.Close()
+	us.AutoMigrate()
+
 	staticC := controllers.NewStatic()
-	usersC := controllers.NewUsers()
+	usersC := controllers.NewUsers(us)
 	galleriesC := controllers.NewGalleries()
 
 	r := mux.NewRouter()
